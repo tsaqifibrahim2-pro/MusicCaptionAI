934835e15df733f5a4d72a4047c2831bcae9a5bf
tsaqifibrahim2-pro <tsaqifibrahim2@gmail.com>
2026-06-05 15:24:05 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './style.css'
 
